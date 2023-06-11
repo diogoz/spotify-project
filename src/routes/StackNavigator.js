d58fe218a1206e37219f5a19207fe2103e9ca1c2
Diogo Sousa <dhesousa77@gmail.com>
2023-06-11 19:44:56 -0300

    Criação do projeto + navegação

---
+++ src/routes/StackNavigator.js
@@ -0,0 +1,81 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Entypo } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import Home from "../screens/Home";
+import Profile from "../screens/Profile";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function BottomTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: "gray",
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          shadowOpacity: 4,
+          shadowRadius: 4,
+          elevation: 4,
+          shadowOffset: {
+            width: 0,
+            height: -4,
+          },
+          borderTopWidth: 0,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: "Home",
+          headerShown: false,
+          tabBarLabelStyle: { color: "white" },
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Entypo name="home" size={24} color="white" />
+            ) : (
+              <AntDesign name="home" size={24} color="white" />
+            ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: "Profile",
+          headerShown: false,
+          tabBarLabelStyle: { color: "white" },
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Ionicons name="person" size={24} color="white" />
+            ) : (
+              <Ionicons name="person-outline" size={24} color="white" />
+            ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="main"
+          component={BottomTabs}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default Navigation;
